f17e0a3889bd066d1b5e61bc49266b53b2ee4937
lx812833 <lx812833@163.com>
2020-10-14 22:21:03 +0800

    存储读取内容

--- webSpider/view/crowller.ts
+++ webSpider/view/crowller.ts
@@ -1,19 +1,50 @@
 import superagent from "superagent";
 import cheerio from "cheerio";
+import fs from "fs";
+import path from "path";
 
 interface Course {
   title: string,
   count: number | string
 }
 
+interface courseResult {
+  time: number,
+  data: Course[]
+}
+
+interface Content {
+  [propName: number]: Course[]
+}
+
 class Crowller {
   private secret = 'x3b174jsx';
   private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`;
+  private filePath = path.resolve(__dirname, "../data/course.json"); // course.json文件路径
 
   async getRawHtml() {
     const result = await superagent.get(this.url);
-    this.getCourseInfo(result.text);
+    return result.text;
   }
+
+  async initSpiderProcess() {
+    const html = await this.getRawHtml();
+    const result = this.getCourseInfo(html);
+    console.log("result", result);
+    const fileContent = this.generateJsonContent(result);
+    fs.writeFileSync(this.filePath, JSON.stringify(fileContent));
+  }
+
+  generateJsonContent(courseInfo: courseResult) {
+    let fileContent: Content = {};
+    if (fs.existsSync(this.filePath)) {
+      // 存在
+      fileContent = JSON.parse(fs.readFileSync(this.filePath, "utf-8"));
+    }
+    fileContent[courseInfo.time] = courseInfo.data; // key-value
+    return fileContent;
+  }
+
   getCourseInfo(html: string) {
     const $ = cheerio.load(html);
     const courseItem = $(".course-item");
@@ -27,10 +58,13 @@ class Crowller {
         count
       })
     })
+    return {
+      time: new Date().getTime(),
+      data: courseInfos
+    }
   }
   constructor() {
-    this.getRawHtml();
-    // superagent
+    this.initSpiderProcess();
   }
 }
 
